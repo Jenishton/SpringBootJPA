f0c6fc7a870e46186a9de60bb0a62fb55757567b
Jenishton Paul <jenishton@gmail.com>
2018-08-25 22:00:12 -0500

    Demo H2 Application

---
+++ src/main/java/com/jenishton/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.jenishton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("BOOT IS UP AND RUNNING");
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into student (id,name, age)
+values(10001,'Jenishton', 42);
+
+insert into student (id,name, age)
+values(10002,'Georgitta', 39);
+
+insert into student (id,name, age)
+values(10003,'Jonathan', 9);
+
+insert into student (id,name, age)
+values(10004,'Joshua', 1);
+
+insert into student (id,name, age)
+values(10005,'Joann', 1);
